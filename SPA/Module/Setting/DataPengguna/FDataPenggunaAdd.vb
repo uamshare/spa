de48509b8ab44dddafa6683ed97619cde1be0097
uam <uamshare@gmail.com>
2015-05-10 10:00:51 +0700

    comit ulang

---
+++ SPA/Module/Setting/DataPengguna/FDataPenggunaAdd.vb
@@ -0,0 +1,145 @@
+﻿Public Class FDataPenggunaAdd
+    Private Model As New MDataPengguna
+    Private Model2 As New MUserGroup
+    Private Model3 As New MUsers
+    Private Sub btnSimpan_Click(sender As Object, e As EventArgs) Handles btnSimpan.Click
+        'MessageBox.Show(Model.EscapeString(TextBox1.Text))
+        'Exit Sub
+        If DataIsValid() Then
+            Dim res As Integer
+
+            Model.mempid = Model.EscapeString(txtKode.Text)
+            Model.mempname = Model.EscapeString(txtNama.Text)
+            Model.memphone1 = Model.EscapeString(txtPhon1.Text)
+            Model.memphone2 = Model.EscapeString(txtPhon2.Text)
+            Model.mempemail = Model.EscapeString(txtEmail.Text)
+            Model.groupid = Model.EscapeString(txtGroupID.Text)
+            Model.username = Model.EscapeString(txtUser.Text)
+            Model.password = Model.EscapeString(txtPass.Text)
+            Model.userid = Model.EscapeString(txtUserID.Text)
+            If txtKode.Text <> "" Then
+                res = Model.UpdateDataPengguna() And Model.UpdateDataUser()
+            Else
+                res = Model.InsertDataUser()
+                If InitializeUser() Then
+                    Model.userid = Model.EscapeString(txtUserID.Text)
+                    res = Model.InsertPengguna()
+                End If
+            End If
+            FDataPengguna.init()
+            Me.Close()
+        End If
+    End Sub
+
+    Private Function DataIsValid() As Boolean
+        DataIsValid = True
+
+        If UserName.Text <> "" Then
+            If txtUser.Text <> UserName.Text Then
+                If Model.IfDataExist3(txtUser.Text) Then
+                    statmsg.Text = "UserName sudah ada, silahkan masukan UserName yang lain."
+                    statmsg.ForeColor = Color.DarkRed
+                    txtUser.Focus()
+                    DataIsValid = False
+                Else
+                    statmsg.Text = "UserName Valid"
+                    statmsg.ForeColor = Color.Green
+                    DataIsValid = True
+                End If
+            End If
+        Else
+            If Model.IfDataExist3(txtUser.Text) Then
+                statmsg.Text = "UserName sudah ada, silahkan masukan UserName yang lain."
+                statmsg.ForeColor = Color.DarkRed
+                txtNama.Focus()
+                DataIsValid = False
+            Else
+                statmsg.Text = "UserName Valid"
+                statmsg.ForeColor = Color.Green
+                DataIsValid = True
+            End If
+        End If
+
+        If txtNama.Text = "" Then
+            statmsg.Text = "Nama tidak boleh kosong !"
+            statmsg.ForeColor = Color.DarkRed
+            txtNama.Focus()
+            DataIsValid = False
+
+        ElseIf cmbGroup.Text = "" Then
+            statmsg.Text = "Group Aksess tidak boleh kosong !"
+            statmsg.ForeColor = Color.DarkRed
+            cmbGroup.Focus()
+            DataIsValid = False
+
+        ElseIf txtUser.Text = "" Then
+            statmsg.Text = "UserName tidak boleh kosong !"
+            statmsg.ForeColor = Color.DarkRed
+            txtUser.Focus()
+            DataIsValid = False
+        ElseIf txtPass.Text = "" Then
+            statmsg.Text = "Password tidak boleh kosong !"
+            statmsg.ForeColor = Color.DarkRed
+            txtPass.Focus()
+            DataIsValid = False
+        ElseIf txtGroupID.Text = "" Then
+            statmsg.Text = "Group Aksess harus di Pilih !"
+            statmsg.ForeColor = Color.DarkRed
+            cmbGroup.Focus()
+            DataIsValid = False
+        End If
+
+        Return DataIsValid
+
+    End Function
+
+    Private Sub txtPhon1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPhon1.KeyPress
+        If Not ((e.KeyChar >= "0" And e.KeyChar <= "9") Or e.KeyChar = vbBack) Then e.Handled = True
+    End Sub
+
+    Private Sub txtPhon2_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtPhon2.KeyPress
+        If Not ((e.KeyChar >= "0" And e.KeyChar <= "9") Or e.KeyChar = vbBack) Then e.Handled = True
+    End Sub
+
+    Private Sub btnBatal_Click(sender As Object, e As EventArgs) Handles btnBatal.Click
+        Me.Close()
+    End Sub
+    Private Sub InitializeCmbGroup()
+        Dim dt As DataTable
+        Try
+            dt = Model2.GetData()
+            'Isi Combo Debit 1
+            cmbGroup.DataSource = dt
+            cmbGroup.ValueMember = "groupid"
+            cmbGroup.DisplayMember = "groupname"
+            cmbGroup.SelectedIndex = -1
+
+        Catch ex As Exception
+            ' tampilkan pesan error
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    Private Function InitializeUser() As Boolean
+        InitializeUser = True
+        Dim dt As DataTable
+        Try
+            dt = Model3.GetData()
+            'Isi Combo Debit 1
+            Dim useridLast As String = dt.Rows.Item(1).Item("LAST_INSERT_ID()")
+            txtUserID.Text = useridLast
+            InitializeUser = True
+
+        Catch ex As Exception
+            ' tampilkan pesan error
+            MessageBox.Show(ex.Message)
+            InitializeUser = False
+        End Try
+    End Function
+    Private Sub cmbGroup_Click(sender As Object, e As EventArgs) Handles cmbGroup.Click
+        InitializeCmbGroup()
+    End Sub
+
+    Private Sub cmbGroup_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbGroup.SelectedIndexChanged
+        txtGroupID.Text = Convert.ToString(cmbGroup.SelectedValue)
+    End Sub
+End Class
---
+++ SPA/Module/Setting/DataPengguna/MDataPengguna.vb
@@ -0,0 +1,103 @@
+﻿Public Class MDataPengguna
+    Inherits CDataAcces
+
+    Public mempid, mempname, mempaddr, memphone1, memphone2, mempemail, memposid, groupid, userid, username, password As String
+    Public dtcreated, dtupdated As String
+
+    Sub New()
+        MyBase.New()
+        BaseQuery = "SELECT a.mempid,a.mempname,a.mempaddr,a.memphone1,a.memphone2,a.mempemail,a.memposid,a.userid,b.username,b.userpassword, c.groupname FROM memp AS a INNER JOIN muser AS b ON a.userid=b.userid INNER JOIN muserg AS c ON b.groupid=c.groupid"
+        SelectQuery = "SELECT a.mempid,a.mempname,a.mempaddr,a.memphone1,a.memphone2,a.mempemail,a.memposid,a.userid,b.username,b.userpassword, c.groupname FROM memp AS a INNER JOIN muser AS b ON a.userid=b.userid INNER JOIN muserg AS c ON b.groupid=c.groupid"
+        TableName = "memp"
+        TableName2 = "muser"
+        PrimaryKey = "mempid"
+        DuplicateData1 = "username"
+    End Sub
+
+    Function FindData(sSearch As String) As DataTable
+        If Not String.IsNullOrEmpty(sSearch) Then
+            Me.WHERE = "WHERE mcusname like '%" & sSearch & "%'"
+        Else
+            Me.WHERE = ""
+        End If
+        Return MyBase.GetData
+    End Function
+    Public Overloads Function InsertPengguna() As Integer
+        Dim dtcreated As String
+        Dim dtupdated As String
+        dtcreated = Format(Date.Now, "yyyy/MM/dd hh:m:s")
+        dtupdated = Format(Date.Now, "yyyy/MM/dd hh:m:s")
+
+        Me.StringSQL = "INSERT INTO " & TableName + "(mempname,mempaddr,memphone1,memphone2,mempemail,userid,dtcreated,dtupdated) VALUES " & _
+                       " ( '" & mempname & "', '" & mempaddr & "', '" & memphone1 & "', '" & memphone2 & "', '" & mempemail & "', " & _
+                       " '" & userid & "', '" & dtcreated & "', '" & dtupdated & "')"
+        Return MyBase.InsertData()
+    End Function
+    Public Overloads Function InsertDataUser() As Integer
+        Dim dtcreated As String
+        Dim dtupdated As String
+        dtcreated = Format(Date.Now, "yyyy/MM/dd hh:m:s")
+        dtupdated = Format(Date.Now, "yyyy/MM/dd hh:m:s")
+
+        Me.StringSQL = "INSERT INTO muser (username,userpassword,groupid,dtcreated,dtupdated) VALUES " & _
+                       " ('" & username & "', '" & password & "',  '" & groupid & "', '" & dtcreated & "', '" & dtupdated & "')"
+        Return MyBase.InsertData()
+    End Function
+
+
+    Public Overloads Function UpdateDataPengguna() As Integer
+        Dim dtupdated As String
+        dtupdated = Format(Date.Now, "yyyy/MM/dd hh:m:s")
+
+        Me.StringSQL = "UPDATE " & TableName + " SET mempname ='" & mempname & "',mempaddr ='" & mempaddr & "',memphone1 ='" & memphone1 & "', memphone2 ='" & memphone2 & "', " & _
+            " mempemail ='" & mempemail & "',userid ='" & userid & "', dtupdated ='" & dtupdated & "' WHERE " & PrimaryKey & "=" & mempid
+        Return MyBase.UpdateData()
+    End Function
+    Public Overloads Function UpdateDataUser() As Integer
+        Dim dtupdated As String
+        dtupdated = Format(Date.Now, "yyyy/MM/dd hh:m:s")
+
+        Me.StringSQL = "UPDATE muser SET username ='" & username & "',userpassword ='" & password & "',groupid ='" & groupid & "', dtupdated ='" & dtupdated & "' WHERE userid =" & userid
+        Return MyBase.UpdateData()
+    End Function
+    Function MultipleDeleteData1(ByVal ids() As String) As Integer
+        BeginTrans("attempting delete data, please wait ... ") 'begin transaction
+        Try
+            cmd.Connection = PCONN
+            'cmd.Prepare()
+            If IsArray(ids) And ids.Length > 0 Then
+                For Each mempid As String In ids
+                    If Not String.IsNullOrEmpty(mempid) Then
+                        Me.StringSQL = "DELETE FROM " & TableName + " WHERE " & PrimaryKey & " = '" & mempid & "'"
+                        cmd.CommandText = Me.StringSQL
+                        rowCountAffected += cmd.ExecuteNonQuery()
+                    End If
+                Next
+            End If
+        Catch ex As MySql.Data.MySqlClient.MySqlException
+            MyApplication.ShowStatus("Error " & ex.Number & " has occurred: " & NOTICE_STAT, True, 10000)
+        End Try
+        CommitTrans(" data have been deleted ", "delete") 'Commit All Transaction
+        Return rowCountAffected
+    End Function
+    Function MultipleDeleteData2(ByVal ids() As String) As Integer
+        BeginTrans("attempting delete data, please wait ... ") 'begin transaction
+        Try
+            cmd.Connection = PCONN
+            'cmd.Prepare()
+            If IsArray(ids) And ids.Length > 0 Then
+                For Each userid As String In ids
+                    If Not String.IsNullOrEmpty(userid) Then
+                        Me.StringSQL = "DELETE FROM " & TableName2 + " WHERE userid = '" & userid & "'"
+                        cmd.CommandText = Me.StringSQL
+                        rowCountAffected += cmd.ExecuteNonQuery()
+                    End If
+                Next
+            End If
+        Catch ex As MySql.Data.MySqlClient.MySqlException
+            MyApplication.ShowStatus("Error " & ex.Number & " has occurred: " & NOTICE_STAT, True, 10000)
+        End Try
+        CommitTrans(" data have been deleted ", "delete") 'Commit All Transaction
+        Return rowCountAffected
+    End Function
+End Class
